6d6f540c7023cadc74941dfb5f81b7b96f6fe581
Patrick Walsh <patrick.nilsen.walsh@gmail.com>
2016-04-17 23:35:29 +0700

    Remove the unnecessary logging from index.js

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 module.exports = function (config) {
   var library = config.mock === false ?  // mock is on by default
       require('./trim.production') :
-      require('./trim.development');
+      require('./trim.development')
 
-  if (config.mock) {
-    console.log('TRIM adapter is loading mock data');
-  } else {
-    console.log('TRIM adapter is live');
-  }
-  return library(config.url, config.token, config.debug);
+  return library(config.url, config.token, config.debug)
 }
